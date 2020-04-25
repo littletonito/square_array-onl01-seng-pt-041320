c7aa3a44b88d52a40ba5e842722e89e280cc5cd9
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-04-25 04:52:30 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,40 @@
 
-def square_array(array)
+def square_array(numbers)
   new_array = []
-  array.each do |numb|
-    new_array << (numb ** 2)
-  end
-  new_array
+  numbers.each do 
+    
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def square_array(array)
+#   new_array = []
+#   array.each do |numb|
+#     new_array << (numb ** 2)
+#   end
+#   new_array
+# end
